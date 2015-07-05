5f93213fa0a5183e85a2ec7f48ecf03cfe2b332a
Harscoet Donovan <donovan.harscoet@gmail.com>
2015-07-05 17:21:25 +0200

    Add batch function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var QueryBuilder = require('./lib/querybuilder');
+var QueryBuilder = require('./lib/querybuilder'),
+    vargs        = require('vargs-callback');
 
 var Cassandra = function () {
   this.client = null;
@@ -19,5 +20,11 @@ Cassandra.prototype.UpdateQuery = function (keyspace, client) {
 Cassandra.prototype.SelectQuery = function (keyspace, client) {
   return new QueryBuilder(3, keyspace, client || this.client);
 };
+
+Cassandra.prototype.batch = vargs(function (queries, client, next) {
+  client = client || this.client;
+
+  client.batch(queries.map(function (query) { return query.dump() }), { prepare: true }, next);
+});
 
 module.exports = new Cassandra();
