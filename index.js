a855fa0893b6464b93c94765ee0495b9fe148ca6
Harscoet Donovan <donovan.harscoet@gmail.com>
2016-05-13 18:18:04 +0200

    Consistency in all queries

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ Cassandra.prototype.setClient = function (client) {
   this.client = client;
 };
 
-Cassandra.prototype.InsertQuery = function (keyspace, client) {
-  return new QueryBuilder(1, keyspace, client || this.client);
+Cassandra.prototype.InsertQuery = function (keyspace, client, consistency) {
+  return new QueryBuilder(1, keyspace, client || this.client, consistency);
 };
 
-Cassandra.prototype.UpdateQuery = function (keyspace, client) {
-  return new QueryBuilder(2, keyspace, client || this.client);
+Cassandra.prototype.UpdateQuery = function (keyspace, client, consistency) {
+  return new QueryBuilder(2, keyspace, client || this.client, consistency);
 };
 
 Cassandra.prototype.SelectQuery = function (keyspace, client, consistency) {
   return new QueryBuilder(3, keyspace, client || this.client, consistency);
 };
 
-Cassandra.prototype.DeleteQuery = function (keyspace, client) {
-  return new QueryBuilder(4, keyspace, client || this.client);
+Cassandra.prototype.DeleteQuery = function (keyspace, client, consistency) {
+  return new QueryBuilder(4, keyspace, client || this.client, consistency);
 };
 
 Cassandra.prototype.batch = vargs(function (queries, client, next) {
